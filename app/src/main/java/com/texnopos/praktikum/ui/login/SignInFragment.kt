7472718d4ff391e3fc345e6cb3ea9d7e6223c29b
Пользователь <Biybiruza>
2022-11-12 14:26:24 +0500

    praktika

---
+++ app/src/main/java/com/texnopos/praktikum/ui/login/SignInFragment.kt
@@ -0,0 +1,10 @@
+package com.texnopos.praktikum.ui.login
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.fragment.app.Fragment
+import com.texnopos.praktikum.R
+
+class SignInFragment() : Fragment(R.layout.fragment_sign_in) {
+
+}
